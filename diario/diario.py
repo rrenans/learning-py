35c61bb3ba6e535cf70cdb88a8e54bcbf26692e6
renans04 <renansantos.silva04@outlook.com>
2022-02-19 11:46:05 -0300

    Arquivo com uma documentação explicando a sintaxe e funções do Python

---
+++ diario/diario.py
@@ -0,0 +1,59 @@
+"""
+    0. Obs:
+        type() = função que mostra o tipo do dado. Ex: print(type(1))
+        # (cerquilha) = para comentários
+        \n = quebra de linha
+        bool() = função que retorna True ou False
+        int() = função que pode converter um dado primitivo para inteiro
+        float() = função que pode converter um dado primitivo para flutuante
+
+
+    1. Operadores Aritméticos:
+        + = Adição ou Concatenação (caso de duas strings)
+        - = Subtração
+        * = Multiplicação
+        / = Divisão (resultado com ponto flutuante)
+        // = Divisão (resultado é arredondado (número inteiro))
+        ** = Exponenciação
+        % = Resto da divisão entre um número e outro
+        () = Altera a precedência dos operadores
+
+    2. Tipos básicos:
+        str (string) = "textos que estão dentro de aspas, sejam elas simples ou duplas"
+            Ex: print("Esse é 'meu' texto") = Nesse exemplo mostra que as aspas simples podem aparecer dentro de
+                                                aspas duplas. Complementando, aspas duplas também podem aparecer
+                                                dentro de aspas simples.
+                print("Esse é meu \"texto\"") = Nesse exemplo, a barra invertida serve como um caractere de escape,
+                                                assim é possível ver uma aspa dupla dentro de outra. Mas não é uma
+                                                prática legal nas últimas versões do python. Assim, é preferível que
+                                                utilize a forma do exemplo acima.
+
+        int (inteiro) = 1, 2, 3...
+        float (ponto flutuante) = 1.1, 2.0, 5.3...
+        bool (booleado) = True, False
+
+    3. Variáveis:
+        Iniciar com letra, pode conter números, separar com _ ou letras Maiúsculas, são escritas com letras minúsculas
+        Ex: nome = 'renan'
+            idade = 17
+            altura = 1.80
+            e_maior = idade >= 18
+            data_1 = True
+            data_atual = 2022
+
+    list
+    tupla
+    dicionário
+    desvios condicionais (if, elif, else)
+    laços com while e for
+    expressões booleanas
+    funções
+    parâmetros
+    docstring
+    módulo
+    import
+    pacote
+    empacotamento
+    desempacotamento
+
+"""
