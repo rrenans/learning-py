22d3f73a24a94c9be226c2f71844c3f6a1062ed0
Renan Silva <renansantos.silva04@outlook.com>
2022-05-06 16:12:06 -0300

    Exemplo de documentação para funções, de uma forma um pouco mais simples

---
+++ diario/belasCoisas/DocStrings/funcoes.py
@@ -0,0 +1,33 @@
+"""
+Descrição rápida e simples.
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type
+specimen book. It has survived not only five centuries, but also the leap into
+electronic typesetting, remaining essentially unchanged. It was popularised in
+the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+and more recently with desktop publishing software like Aldus PageMaker
+including versions of Lorem Ipsum.
+"""
+
+
+variavel_1 = 'valor1'
+
+def soma(x, y):
+    """Soma de x e y"""
+    return x + y
+
+def multiplica(x, y, z=None):
+    """Soma x, y, z
+        Multiplica x, y, z. O programador por omitir a variável z caso não tenha
+        necessidade de usá-la
+    """
+    if z:
+        return x * y
+    else:
+        return x * y * z
+
+variavel_2 = 'valor 1'
+variavel_3 = 'valor 2'
+variavel_4 = 'valor 3'
