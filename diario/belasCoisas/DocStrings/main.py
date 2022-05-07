505d79e0838a4029b70c2b78575f1f75a76349ec
Renan Silva <renansantos.silva04@outlook.com>
2022-05-07 00:16:51 -0300

    Arquivo main, para testar na prática como funciona todas as formas de documentações

---
+++ diario/belasCoisas/DocStrings/main.py
@@ -0,0 +1,14 @@
+# import umaLinha
+# import variasLinhas
+# import funcoes
+# import funcoesParte2
+import classes
+# import typehints
+
+# help(umaLinha)
+# help(variasLinhas)
+# help(funcoes.multiplica)
+# print(funcoes.multiplica.__doc__)
+# help(funcoesParte2)
+help(classes)
+# help(typehints)
