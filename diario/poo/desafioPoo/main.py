860a1b17cd2bd1b9b073936d9622aa0fbd7ce584
Renan Silva <renansantos.silva04@outlook.com>
2022-05-08 00:01:48 -0300

    Arquivo main que executa todos os módulos

--- diario/poo/desafioPoo/main.py
+++ diario/poo/desafioPoo/main.py
@@ -23,3 +23,45 @@ Banco será responsável autenticar o cliente e as contas da seguinte maneira:
     * Checar se a conta é daquele banco
 Só será possível sacar se passar na autenticação do banco (descrição acima)
 """
+
+from Cliente import Cliente
+from Banco import Banco
+from ContaCorrente import ContaCorrente
+from ContaPoupanca import ContaPoupanca
+
+banco = Banco()
+
+cliente1 = Cliente('Renan', 18)
+cliente2 = Cliente('Maria', 25)
+cliente3 = Cliente('João', 27)
+
+conta1 = ContaPoupanca(1111, 254136, 0)
+conta2 = ContaCorrente(2222, 521463, 0)
+conta3 = ContaPoupanca(3331, 364125, 0)
+
+cliente1.inserir_conta(conta1)
+cliente2.inserir_conta(conta2)
+cliente3.inserir_conta(conta3)
+
+banco.inserir_clientes(cliente1)
+banco.inserir_contas(conta1)
+
+banco.inserir_clientes(cliente2)
+banco.inserir_contas(conta2)
+
+banco.inserir_clientes(cliente3)
+banco.inserir_contas(conta3)
+
+if banco.autenticar(cliente1):
+    cliente1.conta.depositar(0)
+    cliente1.conta.sacar(10)
+else:
+    print('Cliente não autenticado')
+
+print()
+
+if banco.autenticar(cliente2):
+    cliente2.conta.depositar(0)
+    cliente2.conta.sacar(20) # aqui deveria constar saldo insuficiente, creio que por conta do polimorfismo, isto não acontece
+else:
+    print('Cliente não autenticado')
