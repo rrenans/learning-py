68170d016a19eaf2bef799b08e2b66d53193bd39
Renan Silva <renansantos.silva04@outlook.com>
2022-05-07 23:30:54 -0300

    Classse Cliente que herda Pessoa e possui um método de inserir conta

---
+++ diario/poo/desafioPoo/Cliente.py
@@ -0,0 +1,10 @@
+from Pessoa import Pessoa
+
+
+class Cliente(Pessoa):
+    def __init__(self, nome, idade):
+        super().__init__(nome, idade)
+        self.conta = None
+
+    def inserir_conta(self, conta):
+        self.conta = conta
