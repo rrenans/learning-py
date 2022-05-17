eba97469b243ee706241b2e5a472c5d9084bb2c6
Renan Silva <renansantos.silva04@outlook.com>
2022-05-17 11:15:48 -0300

    Arquivo main -> Entendendo o Jupyter Notebook

---
+++ modulos/jupyter/main.py
@@ -0,0 +1,19 @@
+"""
+Jupyter Notebook é uma aplicação web que permite a execução de código e mtempo
+real, visualização de dados e textos explicativos.
+Cientistas de dados podem criar, executar o código e explicar o processo
+enquanto executam o código e mostram os dados
+
+INSTALANDO
+pip install jupyter
+ou
+
+ENTRANDO NO AMBIENTE
+pipenv shell
+
+INSTALANDO
+pipenv install jupyter
+
+RODANDO
+jupyter notebook
+"""
